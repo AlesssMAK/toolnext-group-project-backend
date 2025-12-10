2756fe0e75bcded4ac4eb0f20d72c81a8e3ce087
Iryna-M-html <ivmohnataya@gmail.com>
2025-12-10 22:26:16 +0200

    with found with toolId

--- src/controllers/bookingsController.js
+++ src/controllers/bookingsController.js
@@ -1,11 +1,19 @@
 import createHttpError from 'http-errors';
 import { Booking } from '../models/booking.js';
 import { User } from '../models/user.js';
+import { Tool } from '../models/tool.js';
 
 export const createBooking = async (req, res, next) => {
-  const { userPhone } = req.body;
+  const { userPhone, toolId } = req.body;
   if (!req.body.userPhone) {
     return next(createHttpError(400, 'Phone is required'));
+  }
+  if (!toolId) {
+    return next(createHttpError(400, 'toolsId is required'));
+  }
+  const tool = await Tool.findById(toolId);
+  if (!tool) {
+    return next(createHttpError(404, 'Tool not found'));
   }
   const existingUser = await User.findOne({ phone: userPhone });
 
