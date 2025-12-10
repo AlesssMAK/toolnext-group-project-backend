ef9ce30efd8a60707cfbad65e07679c03caadf6e
Iryna-M-html <ivmohnataya@gmail.com>
2025-12-10 21:16:59 +0200

    add booking routes +import bookingroutes w serwer.js

--- src/routes/bookingsRoutes.js
+++ src/routes/bookingsRoutes.js
@@ -1,1 +1,11 @@
-///Test 2
+import { Router } from 'express';
+import { celebrate } from 'celebrate';
+import { createBookingSchema } from '../validations/bookingsValidation.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+const router = Router();
+router.post(
+  '/bookings',
+  celebrate(createBookingSchema),
+  ctrlWrapper(createBooking),
+);
+export default router;
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
+import bookingsRoutes from './routes/bookingsRoutes.js';
 
 const app = express();
 const PORT = 3000;
@@ -13,7 +14,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(logger);
-
+app.use(bookingsRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
