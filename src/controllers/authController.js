b4312d4fd899d55d413792225d05bb17de3eeaf2
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2025-12-11 15:18:48 +0200

    LoginUser

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,7 @@ import createHttpError from "http-errors";
 
 import { createSession, setSessionCookies  } from "../services/auth.js";
 import { User } from "../models/user.js";
+import { Session } from "../models/session.js";
 
 export const registerUser = async (req, res, next) => {
   const { name, email, password} = req.body;
@@ -31,5 +32,38 @@ export const registerUser = async (req, res, next) => {
         email: newUser.email,
         name: newUser.name,
       }
+  });
+};
+
+export const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // const username = await User.findOne({ name: name._id});
+  // console.log(username);
+  // if (!username) {
+  //   return next(createHttpError(401, 'Invalid name or password'));
+  // }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(createHttpError(401, 'Invalid email or password'));
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return next(createHttpError(401, 'Invalid email or password'));
+  }
+
+  await Session.deleteOne({ userId: user._id });
+  const newSession = await createSession(user._id);
+
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({
+    user: {
+      id: user._id,
+      email: user.email,
+      name: user.name,
+    }
   });
 };
