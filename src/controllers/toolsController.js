4f27c8cb551db1d87b9a986e044c7c2c8f6cbac1
maryna <mariam.17melchynscka@gmail.com>
2025-12-12 22:53:55 +0200

    add-post-patch-utilCloud

--- src/controllers/toolsController.js
+++ src/controllers/toolsController.js
@@ -1,5 +1,7 @@
+import createHttpError from 'http-errors';
 import { Tool } from '../models/tool.js';
 import mongoose from 'mongoose'; 
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const getTools = async (req, res) => {
     const { page = 1, limit = 10, categories, search } = req.query;
@@ -48,4 +50,84 @@ export const getTools = async (req, res) => {
     totalPages,
     tools,
     });
+};
+
+export const createTool = async (req, res, next) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ message: 'Image is required' });
+        }
+        const cloudinaryResponse = await saveFileToCloudinary(req.file.buffer);
+
+        const { specifications, ...rest } = req.body;
+
+        let parsedSpecs = {};
+        if (specifications) {
+            try {
+                parsedSpecs = typeof specifications === 'string'
+                    ? JSON.parse(specifications)
+                    : specifications;
+            } catch  {
+                return res.status(400).json({ message: 'Invalid JSON in specifications' });
+            }
+        }
+
+        const newTool = await Tool.create({
+            ...rest,
+            owner: req.user._id ,
+            images: cloudinaryResponse.secure_url,
+            specifications: parsedSpecs,
+        });
+        
+
+        res.status(201).json({ status: 'success', data: newTool });
+    } catch (error) {
+        next(error);
+    }
+    
+};
+
+export const updateTool = async (req, res, next) => {
+    try {
+        const { toolId } = req.params;
+        const userId = req.user._id;
+        
+        const tool = await Tool.findById(toolId);
+
+        if (!tool) {
+            return next(createHttpError(404, 'Tool not found'));
+        };
+        if (tool.owner.toString() !== userId.toString()) {
+            return next(createHttpError(403, 'Access denied: You are not the owner'));
+        };
+
+        const { specifications, ...rest } = req.body;
+        const updateData = { ...rest };
+        
+        if (req.file) {
+            const cloudinaryResponse = await saveFileToCloudinary(req.file.buffer);
+            updateData.images = cloudinaryResponse.secure_url;
+        } 
+        if (specifications) {
+            try {
+                updateData.specifications = typeof specifications === 'string'
+                    ? JSON.parse(specifications)
+                    : specifications;
+            } catch {
+                return next(createHttpError(400, 'Invalid JSON in specifications'));
+            }
+        } 
+        const updatedTool = await Tool.findByIdAndUpdate(
+            toolId,
+            updateData,
+            { new: true, runValidators: true }
+        );
+
+        res.status(200).json({
+            status: 'success',
+            data: updatedTool,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
--- src/routes/toolsRoutes.js
+++ src/routes/toolsRoutes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { getTools } from '../controllers/toolsController.js';
+import { getTools, createTool, updateTool} from '../controllers/toolsController.js';
 import { celebrate } from 'celebrate';
-import { getToolsSchema } from '../validations/toolsValidation.js';
+import { getToolsSchema, createToolSchema, updateToolSchema } from '../validations/toolsValidation.js';
+import { upload } from '../middleware/multer.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const toolRouters = Router();
 
 toolRouters.get("/api/tools", celebrate(getToolsSchema), getTools);
+
+toolRouters.post("/api/tools", authenticate, upload.single("image"), celebrate(createToolSchema), createTool);
+
+toolRouters.patch("/api/tools/:toolId", authenticate, upload.single("image"), celebrate(updateToolSchema), updateTool);
 
 export default toolRouters;
