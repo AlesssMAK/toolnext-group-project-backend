e623eb35ee1b6f7a59c31a9b1e0e155729ee5c76
Iryna-M-html <ivmohnataya@gmail.com>
2025-12-14 14:09:50 +0200

    fixed authenticate.js

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,14 +1,13 @@
+import createHttpError from 'http-errors';
+import { Session } from '../models/session.js';
+import { User } from '../models/user.js';
 
-import createHttpError from "http-errors";
-import { Session } from "../models/session.js";
-import { User } from "../models/user.js";
-
-export const authenticate = async (req, next) => {
-
+export const authenticate = async (req, res, next) => {
+  try {
     if (!req.cookies.accessToken) {
       next(createHttpError(401, 'Missing access token'));
       return;
-    };
+    }
 
     const session = await Session.findOne({
       accessToken: req.cookies.accessToken,
@@ -16,18 +15,19 @@ export const authenticate = async (req, next) => {
     if (!session) {
       next(createHttpError(401, 'Session not found'));
       return;
-    };
+    }
 
-    const isAccessTokenExpired = new Date() > new Date(session.accessTokenValidUntil);
+    const isAccessTokenExpired =
+      new Date() > new Date(session.accessTokenValidUntil);
     if (isAccessTokenExpired) {
       return next(createHttpError(401, 'Access token expired'));
-    };
+    }
 
     const user = await User.findById(session.userId);
     if (!user) {
       next(createHttpError(401));
       return;
-    };
+    }
 
     req.user = {
       id: user._id,
@@ -36,4 +36,7 @@ export const authenticate = async (req, next) => {
       name: user.name,
     };
     next();
+  } catch (error) {
+    next(error);
+  }
 };
