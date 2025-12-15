eae7e9e9d789b7a29fab7cdaa4403b30f4626046
Iryna-M-html <ivmohnataya@gmail.com>
2025-12-15 10:55:08 +0200

    categorieRoutes fixed

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-import { logger } from "./middleware/logger.js";
-import { errorHandler } from "./middleware/errorHandler.js";
-import { notFoundHandler } from "./middleware/notFoundHandler.js";
-import { errors } from "celebrate";
-import { connectMongoDB } from "./db/connectMongoDB.js";
+import { logger } from './middleware/logger.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errors } from 'celebrate';
+import { connectMongoDB } from './db/connectMongoDB.js';
 
-import toolRouters from "./routes/toolsRoutes.js";
-import bookingsRoutes from "./routes/bookingsRoutes.js";
-import feedbackRoutes from "./routes/feedbackRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import categoriesRoutes from "./routes/categoriesRoutes.js";
+import toolRouters from './routes/toolsRoutes.js';
+import bookingsRoutes from './routes/bookingsRoutes.js';
+import feedbackRoutes from './routes/feedbackRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import categoriesRoutes from './routes/categoriesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,10 +22,12 @@ const PORT = process.env.PORT || 3000;
 // middlewares
 app.use(logger);
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 // routes
@@ -36,7 +38,7 @@ app.use(feedbackRoutes);
 app.use(userRoutes);
 
 // 🔥 PUBLIC ENDPOINT
-app.use("/api/categories", categoriesRoutes);
+app.use(categoriesRoutes);
 
 // errors
 app.use(notFoundHandler);
@@ -50,10 +52,9 @@ const startServer = async () => {
       console.log(`✅ Server is running on port ${PORT}`);
     });
   } catch (error) {
-    console.error("❌ Failed to connect to MongoDB:", error.message);
+    console.error('❌ Failed to connect to MongoDB:', error.message);
     process.exit(1);
   }
 };
 
 startServer();
-
