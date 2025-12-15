bf2894c4ceffa5a546f8bedaa1ca2c8ff0b94ebe
Iryna-M-html <ivmohnataya@gmail.com>
2025-12-15 13:36:32 +0200

    fixed userid create+patchtools

--- src/controllers/toolsController.js
+++ src/controllers/toolsController.js
@@ -1,161 +1,159 @@
 import createHttpError from 'http-errors';
-import mongoose from 'mongoose'; 
+import mongoose from 'mongoose';
 import { Tool } from '../models/tool.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
-
 export const getTools = async (req, res) => {
-    const { page = 1, limit = 10, categories, search } = req.query;
+  const { page = 1, limit = 10, categories, search } = req.query;
 
-    const pageNumber = parseInt(page, 10);
-    const limitNumber = parseInt(limit, 10);
+  const pageNumber = parseInt(page, 10);
+  const limitNumber = parseInt(limit, 10);
 
-    const skip = (pageNumber - 1) * limitNumber;
+  const skip = (pageNumber - 1) * limitNumber;
 
-    const filters = {};
-     if (categories) {
-        const categoryIdsString = categories.split(',');
-        
-        
-        const categoryObjectIds = categoryIdsString.map(id => {
-            
-            if (mongoose.Types.ObjectId.isValid(id)) {
-                return new mongoose.Types.ObjectId(id);
-            }
-            return null; 
-        }).filter(id => id !== null); 
+  const filters = {};
+  if (categories) {
+    const categoryIdsString = categories.split(',');
 
-        if (categoryObjectIds.length > 0) {
-            filters.category = { $in: categoryObjectIds };
-        } else if (categories.length > 0 && categoryObjectIds.length === 0) {
-            
-            filters.category = { $in: [] }; 
+    const categoryObjectIds = categoryIdsString
+      .map((id) => {
+        if (mongoose.Types.ObjectId.isValid(id)) {
+          return new mongoose.Types.ObjectId(id);
         }
-    };
+        return null;
+      })
+      .filter((id) => id !== null);
 
-    if (search) {
-        filters.name = { $regex: search, $options: 'i' }; 
+    if (categoryObjectIds.length > 0) {
+      filters.category = { $in: categoryObjectIds };
+    } else if (categories.length > 0 && categoryObjectIds.length === 0) {
+      filters.category = { $in: [] };
     }
-    const toolsQuery = Tool.find(filters);
+  }
 
-    const [totalItems, tools] = await Promise.all([
-        toolsQuery.clone().countDocuments(),
-        toolsQuery.skip(skip).limit(limitNumber),
-    ]);
-    const totalPages = Math.ceil(totalItems / limitNumber);
+  if (search) {
+    filters.name = { $regex: search, $options: 'i' };
+  }
+  const toolsQuery = Tool.find(filters);
 
-    res.status(200).json({
+  const [totalItems, tools] = await Promise.all([
+    toolsQuery.clone().countDocuments(),
+    toolsQuery.skip(skip).limit(limitNumber),
+  ]);
+  const totalPages = Math.ceil(totalItems / limitNumber);
+
+  res.status(200).json({
     page: pageNumber,
     limit: limitNumber,
     totalItems,
     totalPages,
     tools,
-    });
+  });
 };
 
 export const createTool = async (req, res, next) => {
-    try {
-        if (!req.file) {
-            return res.status(400).json({ message: 'Image is required' });
-        }
-        const cloudinaryResponse = await saveFileToCloudinary(req.file.buffer);
-
-        const { specifications, ...rest } = req.body;
-
-        let parsedSpecs = {};
-        if (specifications) {
-            try {
-                parsedSpecs = typeof specifications === 'string'
-                    ? JSON.parse(specifications)
-                    : specifications;
-            } catch  {
-                return res.status(400).json({ message: 'Invalid JSON in specifications' });
-            }
-        }
-
-        const newTool = await Tool.create({
-            ...rest,
-            owner: req.user._id ,
-            images: cloudinaryResponse.secure_url,
-            specifications: parsedSpecs,
-        });
-        
-
-        res.status(201).json({ status: 'success', data: newTool });
-    } catch (error) {
-        next(error);
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: 'Image is required' });
     }
-    
+    const cloudinaryResponse = await saveFileToCloudinary(req.file.buffer);
+
+    const { specifications, ...rest } = req.body;
+
+    let parsedSpecs = {};
+    if (specifications) {
+      try {
+        parsedSpecs =
+          typeof specifications === 'string'
+            ? JSON.parse(specifications)
+            : specifications;
+      } catch {
+        return res
+          .status(400)
+          .json({ message: 'Invalid JSON in specifications' });
+      }
+    }
+
+    const newTool = await Tool.create({
+      ...rest,
+      owner: req.user.id,
+      images: cloudinaryResponse.secure_url,
+      specifications: parsedSpecs,
+    });
+
+    res.status(201).json({ status: 'success', data: newTool });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateTool = async (req, res, next) => {
-    try {
-        const { toolId } = req.params;
-        const userId = req.user._id;
-        
-        const tool = await Tool.findById(toolId);
-
-        if (!tool) {
-            return next(createHttpError(404, 'Tool not found'));
-        };
-        if (tool.owner.toString() !== userId.toString()) {
-            return next(createHttpError(403, 'Access denied: You are not the owner'));
-        };
-
-        const { specifications, ...rest } = req.body;
-        const updateData = { ...rest };
-        
-        if (req.file) {
-            const cloudinaryResponse = await saveFileToCloudinary(req.file.buffer);
-            updateData.images = cloudinaryResponse.secure_url;
-        } 
-        if (specifications) {
-            try {
-                updateData.specifications = typeof specifications === 'string'
-                    ? JSON.parse(specifications)
-                    : specifications;
-            } catch {
-                return next(createHttpError(400, 'Invalid JSON in specifications'));
-            }
-        } 
-        const updatedTool = await Tool.findByIdAndUpdate(
-            toolId,
-            updateData,
-            { new: true, runValidators: true }
-        );
-
-        res.status(200).json({
-            status: 'success',
-            data: updatedTool,
-        });
-    } catch (error) {
-        next(error);
-    }
-};
-
-export const getToolById = async (req, res, next) => {
+  try {
     const { toolId } = req.params;
+    const userId = req.user.id;
+
     const tool = await Tool.findById(toolId);
 
     if (!tool) {
+      return next(createHttpError(404, 'Tool not found'));
+    }
+    if (tool.owner.toString() !== userId.toString()) {
+      return next(createHttpError(403, 'Access denied: You are not the owner'));
+    }
+
+    const { specifications, ...rest } = req.body;
+    const updateData = { ...rest };
+
+    if (req.file) {
+      const cloudinaryResponse = await saveFileToCloudinary(req.file.buffer);
+      updateData.images = cloudinaryResponse.secure_url;
+    }
+    if (specifications) {
+      try {
+        updateData.specifications =
+          typeof specifications === 'string'
+            ? JSON.parse(specifications)
+            : specifications;
+      } catch {
+        return next(createHttpError(400, 'Invalid JSON in specifications'));
+      }
+    }
+    const updatedTool = await Tool.findByIdAndUpdate(toolId, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: updatedTool,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getToolById = async (req, res, next) => {
+  const { toolId } = req.params;
+  const tool = await Tool.findById(toolId);
+
+  if (!tool) {
     next(createHttpError(404, 'Tool not found'));
     return;
-    };
+  }
 
-    res.status(200).json(tool);
+  res.status(200).json(tool);
 };
 
 export const deleteTool = async (req, res, next) => {
-    const { toolId } = req.params;
+  const { toolId } = req.params;
 
-    const tool = await Tool.findOneAndDelete({
+  const tool = await Tool.findOneAndDelete({
     _id: toolId,
-    });
+  });
 
-    if (!tool) {
+  if (!tool) {
     next(createHttpError(404, 'Tool not found'));
     return;
-    };
-    res.status(200).json(tool);
+  }
+  res.status(200).json(tool);
 };
-
