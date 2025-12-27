f78e0ece10e476ab121b5ee27a3c2ea614e7f44b
Iryna-M-html <ivmohnataya@gmail.com>
2025-12-28 00:11:56 +0200

    /users/{userId}/feedbacks

--- src/swagger/paths/users.js
+++ src/swagger/paths/users.js
@@ -179,4 +179,122 @@
  *           application/json:
  *             schema:
  *               $ref: '#/components/schemas/Error'
+ *
+ * /api/users/{userId}/feedbacks:
+ *   get:
+ *     summary: Get feedbacks for a specific user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: 64f0c2a9b9a1c2a1a1234567
+ *         description: ID of the user whose feedbacks are requested
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *         description: Page number for pagination
+ *       - in: query
+ *         name: perPage
+ *         schema:
+ *           type: integer
+ *           default: 10
+ *         description: Number of feedbacks per page
+ *       - in: query
+ *         name: sortBy
+ *         schema:
+ *           type: string
+ *           default: createdAt
+ *         description: Field to sort feedbacks by
+ *       - in: query
+ *         name: sortOrder
+ *         schema:
+ *           type: string
+ *           enum: [asc, desc]
+ *           default: desc
+ *         description: Sort order
+ *     responses:
+ *       200:
+ *         description: List of feedbacks with pagination info
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     feedbacks:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           _id:
+ *                             type: string
+ *                             example: 64f0c2a9b9a1c2a1a1234568
+ *                           userId:
+ *                             type: object
+ *                             properties:
+ *                               _id:
+ *                                 type: string
+ *                                 example: 64f0c2a9b9a1c2a1a1234567
+ *                               name:
+ *                                 type: string
+ *                                 example: John Doe
+ *                           toolId:
+ *                             type: object
+ *                             properties:
+ *                               _id:
+ *                                 type: string
+ *                                 example: 64f0c2b0b9a1c2a1a1234569
+ *                               name:
+ *                                 type: string
+ *                                 example: Hammer
+ *                           rating:
+ *                             type: integer
+ *                             example: 5
+ *                           comment:
+ *                             type: string
+ *                             example: Very useful tool
+ *                           createdAt:
+ *                             type: string
+ *                             format: date-time
+ *                             example: 2025-12-28T12:34:56Z
+ *                     pagination:
+ *                       type: object
+ *                       properties:
+ *                         currentPage:
+ *                           type: integer
+ *                           example: 1
+ *                         perPage:
+ *                           type: integer
+ *                           example: 10
+ *                         totalFeedbacks:
+ *                           type: integer
+ *                           example: 25
+ *                         totalPages:
+ *                           type: integer
+ *                           example: 3
+ *       400:
+ *         description: Invalid user ID format
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *             example:
+ *               statusCode: 400
+ *               message: Invalid user ID format
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *             example:
+ *               statusCode: 404
+ *               message: User not found
  */
